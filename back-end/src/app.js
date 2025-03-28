24fe902f2bd9d5dfc0660c95d88e253df28a0f04
Fausto G. Cintra <professor@faustocintra.com.br>
2025-03-27 21:59:23 -0300

    (27/03) Upload inicial

---
+++ back-end/src/app.js
@@ -0,0 +1,39 @@
+import dotenv from 'dotenv'
+dotenv.config() // Carrega as variáveis de ambiente do arquivo .env
+
+import express, { json, urlencoded } from 'express'
+import cookieParser from 'cookie-parser'
+import logger from 'morgan'
+
+import indexRouter from './routes/index.js'
+//import usersRouter from './routes/users.js'
+
+const app = express()
+
+import cors from 'cors'
+
+app.use(cors({
+  origin: process.env.ALLOWED_ORIGINS.split(','),
+  credentials: true   // Grava cookie no front-end
+}))
+
+app.use(logger('dev'))
+app.use(json())
+app.use(urlencoded({ extended: false }))
+app.use(cookieParser())
+
+app.use('/', indexRouter)
+//app.use('/users', usersRouter)
+
+/*********** ROTAS DA API **************/
+
+import carsRouter from './routes/cars.js'
+app.use('/cars', carsRouter)
+
+import customersRouter from './routes/customers.js'
+app.use('/customers', customersRouter)
+
+import usersRouter from './routes/users.js'
+app.use('/users', usersRouter)
+
+export default app
---
+++ front-end/src/routes/AppRoutes.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+
+import Homepage from '../pages/Homepage'
+
+import CarForm from '../pages/car/CarForm'
+import CarList from '../pages/car/CarList'
+
+import CustomerForm from '../pages/customer/CustomerForm'
+import CustomerList from '../pages/customer/CustomerList'
+
+import UserList from '../pages/user/UserList'
+import UserForm from '../pages/user/UserForm'
+
+import About from '../pages/About'
+
+import Login from '../pages/Login'
+
+export default function AppRoutes() {
+  return <Routes>
+    <Route path="/" element={ <Homepage /> } />
+
+    <Route path="/login" element={ <Login /> } />
+
+    <Route path="/cars" element={ <CarList /> } />
+    <Route path="/cars/new" element={ <CarForm /> } />
+    <Route path="/cars/:id" element={ <CarForm /> } />
+
+    <Route path="/customers" element={ 
+      <CustomerList /> 
+    } />
+    
+    <Route path="/customers/new" element={ <CustomerForm />} />
+    <Route path="/customers/:id" element={ <CustomerForm />  } />
+
+    <Route path="/users" element={ <UserList /> } />
+    <Route path="/users/new" element={ <UserForm /> } />
+    <Route path="/users/:id" element={ <UserForm /> } />
+
+    {/* 11. Crie uma rota para esse componente. */}
+    <Route path="/about" element={ <About /> } />
+  </Routes>
+}
