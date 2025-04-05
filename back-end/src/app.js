f667b0d421fd586f86fdc3c00a689d89e68343c5
Silvio Junior <juninho.1401@hotmail.com>
2025-04-04 22:10:40 -0300

    (04/04) Segurança de CORS e Senhas

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,26 +1,31 @@
-import dotenv from 'dotenv'
-dotenv.config() // Carrega as variáveis de ambiente do arquivo .env
+import dotenv from "dotenv";
+dotenv.config(); // Carrega as variáveis de ambiente do arquivo .env
 
-import express, { json, urlencoded } from 'express'
-import cookieParser from 'cookie-parser'
-import logger from 'morgan'
+import express, { json, urlencoded } from "express";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import cors from "cors";
 
-const app = express()
+const app = express();
 
-app.use(logger('dev'))
-app.use(json())
-app.use(urlencoded({ extended: false }))
-app.use(cookieParser())
+app.use(logger("dev"));
+app.use(json());
+app.use(urlencoded({ extended: false }));
+app.use(cookieParser());
+// Configurando o CORS para que o back-end aceite
+// requisições vindas das origens indicadas pela
+// variável de ambiente ALLOWED_ORIGINS
+app.use(cors({ origin: process.env.ALLOWED_ORIGINS.split(",") }));
 
 /*********** ROTAS DA API **************/
 
-import carsRouter from './routes/cars.js'
-app.use('/cars', carsRouter)
+import carsRouter from "./routes/cars.js";
+app.use("/cars", carsRouter);
 
-import customersRouter from './routes/customers.js'
-app.use('/customers', customersRouter)
+import customersRouter from "./routes/customers.js";
+app.use("/customers", customersRouter);
 
-import usersRouter from './routes/users.js'
-app.use('/users', usersRouter)
+import usersRouter from "./routes/users.js";
+app.use("/users", usersRouter);
 
-export default app
+export default app;
--- back-end/src/controllers/users.js
+++ back-end/src/controllers/users.js
@@ -1,163 +1,178 @@
-import prisma from '../database/client.js'
-import jwt from 'jsonwebtoken'
+import prisma from "../database/client.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
-const controller = {}     // Objeto vazio
+const controller = {}; // Objeto vazio
 
-controller.create = async function(req, res) {
-  try {
+controller.create = async function (req, res) {
+    try {
+        // Verifica se existe o campo "password" e o
+        // criptografa antes de criar o novo usuário
+        if (req.body.password)
+            req.body.password = await bcrypt.hash(req.body.password, 12);
 
-    await prisma.user.create({ data: req.body })
+        await prisma.user.create({ data: req.body });
 
-    // HTTP 201: Created
-    res.status(201).end()
-  }
-  catch(error) {
-    console.error(error)
+        // HTTP 201: Created
+        res.status(201).end();
+    } catch (error) {
+        console.error(error);
 
-    // HTTP 500: Internal Server Error
-    res.status(500).end()
-  }
-}
-
-controller.retrieveAll = async function(req, res) {
-  try {
-    const result = await prisma.user.findMany()
-
-    // HTTP 200: OK (implícito)
-    res.send(result)
-  }
-  catch(error) {
-    console.error(error)
-
-    // HTTP 500: Internal Server Error
-    res.status(500).end()
-  }
-}
-
-controller.retrieveOne = async function(req, res) {
-  try {
-    const result = await prisma.user.findUnique({
-      where: { id: Number(req.params.id) }
-    })
-
-    // Encontrou ~> retorna HTTP 200: OK (implícito)
-    if(result) res.send(result)
-    // Não encontrou ~> retorna HTTP 404: Not Found
-    else res.status(404).end()
-  }
-  catch(error) {
-    console.error(error)
-
-    // HTTP 500: Internal Server Error
-    res.status(500).end()
-  }
-}
-
-controller.update = async function(req, res) {
-  try {
-
-    const result = await prisma.user.update({
-      where: { id: Number(req.params.id) },
-      data: req.body
-    })
-
-    // Encontrou e atualizou ~> HTTP 204: No Content
-    if(result) res.status(204).end()
-    // Não encontrou (e não atualizou) ~> HTTP 404: Not Found
-    else res.status(404).end()
-  }
-  catch(error) {
-    console.error(error)
-
-    // HTTP 500: Internal Server Error
-    res.status(500).end()
-  }
-}
-
-controller.delete = async function(req, res) {
-  try {
-    await prisma.user.delete({
-      where: { id: Number(req.params.id) }
-    })
-
-    // Encontrou e excluiu ~> HTTP 204: No Content
-    res.status(204).end()
-  }
-  catch(error) {
-    if(error?.code === 'P2025') {
-      // Não encontrou e não excluiu ~> HTTP 404: Not Found
-      res.status(404).end()
+        // HTTP 500: Internal Server Error
+        res.status(500).end();
     }
-    else {
-      // Outros tipos de erro
-      console.error(error)
+};
 
-      // HTTP 500: Internal Server Error
-      res.status(500).end()
+controller.retrieveAll = async function (req, res) {
+    try {
+        // Omite o campo "password" do resultado
+        // por questão de segurança
+        const result = await prisma.user.findMany({ omit: { password: true } });
+
+        // HTTP 200: OK (implícito)
+        res.send(result);
+    } catch (error) {
+        console.error(error);
+
+        // HTTP 500: Internal Server Error
+        res.status(500).end();
     }
-  }
-}
+};
 
-controller.login = async function(req, res) {
-  try {
+controller.retrieveOne = async function (req, res) {
+    try {
+        const result = await prisma.user.findUnique({
+            // Omite o campo "password" do resultado
+            // por questão de segurança
+            omit: { password: true },
+            where: { id: Number(req.params.id) },
+        });
 
-      // Busca o usuário no BD usando o valor dos campos
-      // "username" OU "email"
-      const user = await prisma.user.findFirst({
-        where: {
-          OR: [
-            { username: req.body?.username },
-            { email: req.body?.email }
-          ]
+        // Encontrou ~> retorna HTTP 200: OK (implícito)
+        if (result) res.send(result);
+        // Não encontrou ~> retorna HTTP 404: Not Found
+        else res.status(404).end();
+    } catch (error) {
+        console.error(error);
+
+        // HTTP 500: Internal Server Error
+        res.status(500).end();
+    }
+};
+
+controller.update = async function (req, res) {
+    try {
+        // Verifica se existe o campo "password" e o
+        // criptografa antes de alterar o usuário
+        if (req.body.password)
+            req.body.password = await bcrypt.hash(req.body.password, 12);
+
+        const result = await prisma.user.update({
+            where: { id: Number(req.params.id) },
+            data: req.body,
+        });
+
+        // Encontrou e atualizou ~> HTTP 204: No Content
+        if (result) res.status(204).end();
+        // Não encontrou (e não atualizou) ~> HTTP 404: Not Found
+        else res.status(404).end();
+    } catch (error) {
+        console.error(error);
+
+        // HTTP 500: Internal Server Error
+        res.status(500).end();
+    }
+};
+
+controller.delete = async function (req, res) {
+    try {
+        await prisma.user.delete({
+            where: { id: Number(req.params.id) },
+        });
+
+        // Encontrou e excluiu ~> HTTP 204: No Content
+        res.status(204).end();
+    } catch (error) {
+        if (error?.code === "P2025") {
+            // Não encontrou e não excluiu ~> HTTP 404: Not Found
+            res.status(404).end();
+        } else {
+            // Outros tipos de erro
+            console.error(error);
+
+            // HTTP 500: Internal Server Error
+            res.status(500).end();
         }
-      })
+    }
+};
 
-      // Se o usuário não for encontrado, retorna
-      // HTTP 401: Unauthorized
-      if(! user) return res.status(401).end()
+controller.login = async function (req, res) {
+    try {
+        // Busca o usuário no BD usando o valor dos campos
+        // "username" OU "email"
+        const user = await prisma.user.findFirst({
+            where: {
+                OR: [
+                    { username: req.body?.username },
+                    { email: req.body?.email },
+                ],
+            },
+        });
 
-      // Usuário encontrado, vamos conferir a senha
-      let passwordIsValid
-      if(req.body?.username === 'admin' && req.body?.password === 'admin123') passwordIsValid = true
-      else passwordIsValid = user.password === req.body?.password
+        // Se o usuário não for encontrado, retorna
+        // HTTP 401: Unauthorized
+        if (!user) return res.status(401).end();
 
-      // Se a senha estiver errada, retorna
-      // HTTP 401: Unauthorized
-      if(! passwordIsValid) return res.status(401).end()
+        // Usuário encontrado, vamos conferir a senha
+        let passwordIsValid;
 
-      // Usuário e senha OK, passamos ao procedimento de gerar o token
-      const token = jwt.sign(
-        user,                       // Dados do usuário
-        process.env.TOKEN_SECRET,   // Senha para criptografar o token
-        { expiresIn: '24h' }        // Prazo de validade do token
-      )
+        // REMOVENDO VULNERABILIDADE DE AUTENTICAÇÃO FIXA
+        // if(req.body?.username === 'admin' && req.body?.password === 'admin123') passwordIsValid = true
+        // else passwordIsValid = user.password === req.body?.password
+        // passwordIsValid = user.password === req.body?.password;
 
-      // Formamos o cookie para enviar ao front-end
-      res.cookie(process.env.AUTH_COOKIE_NAME, token, {
-        httpOnly: true, // O cookie ficará inacessível para o JS no front-end
-        secure: true,   // O cookie será criptografado em conexões https
-        sameSite: 'None',
-        path: '/',
-        maxAge: 24 * 60 * 60 * 100  // 24h
-      })
+        // Chamando bcrypt.compare() para verificar se o hash da senha
+        // enviada coincide com o hash da senha armazenada no BD
+        passwordIsValid = await bcrypt.compare(
+            req.body?.password,
+            user.password
+        );
 
-      // Retorna o token e o usuário autenticado com
-      // HTTP 200: OK (implícito)
-      res.send({token, user})
+        // Se a senha estiver errada, retorna
+        // HTTP 401: Unauthorized
+        if (!passwordIsValid) return res.status(401).end();
 
-  }
-  catch(error) {
-    console.error(error)
+        // Usuário e senha OK, passamos ao procedimento de gerar o token
+        const token = jwt.sign(
+            user, // Dados do usuário
+            process.env.TOKEN_SECRET, // Senha para criptografar o token
+            { expiresIn: "24h" } // Prazo de validade do token
+        );
 
-    // HTTP 500: Internal Server Error
-    res.status(500).end()
-  }
-}
+        // Formamos o cookie para enviar ao front-end
+        res.cookie(process.env.AUTH_COOKIE_NAME, token, {
+            httpOnly: true, // O cookie ficará inacessível para o JS no front-end
+            secure: true, // O cookie será criptografado em conexões https
+            sameSite: "None",
+            path: "/",
+            maxAge: 24 * 60 * 60 * 100, // 24h
+        });
 
-controller.me = function(req, res) {
-  // Retorna as informações do usuário autenticado
-  // HTTP 200: OK (implícito)
-  res.send(req?.authUser)
-}
+        // Retorna o token e o usuário autenticado com
+        // HTTP 200: OK (implícito)
+        res.send({ token, user });
+    } catch (error) {
+        console.error(error);
 
-export default controller
+        // HTTP 500: Internal Server Error
+        res.status(500).end();
+    }
+};
+
+controller.me = function (req, res) {
+    // Retorna as informações do usuário autenticado
+    // HTTP 200: OK (implícito)
+    res.send(req?.authUser);
+};
+
+export default controller;
